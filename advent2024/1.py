9846ddec6444504cab2f712ee484ad5c5b20c203
Sebastian Sarco <63447579+bodoque007@users.noreply.github.com>
2024-12-01 20:48:52 -0300

    Create 1.py

---
+++ advent2024/1.py
@@ -0,0 +1,16 @@
+with open("input.txt", "r") as file:
+    firsts = []
+    seconds = []
+    for line in file:
+        a, b = map(int, line.split())
+        firsts.append(a)
+        seconds.append(b)
+    
+    firsts.sort()
+    seconds.sort()
+    pairs = list(zip(firsts, seconds)) 
+    
+    distance = 0
+    for a, b in pairs:
+        distance += abs(a - b)
+    print(distance)
