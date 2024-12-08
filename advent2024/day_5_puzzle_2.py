804fbec161ced7e9f0fc9e19118039c2009422e3
Sebastian Sarco <63447579+bodoque007@users.noreply.github.com>
2024-12-08 01:44:31 -0300

    Update day_5_puzzle_2.py

--- advent2024/day_5_puzzle_2.py
+++ advent2024/day_5_puzzle_2.py
@@ -4,7 +4,7 @@ graph = defaultdict(list)
 operations = []
 dependencies = defaultdict(set)
 
-with open("input.txt", "r") as file:
+with open("input2.txt", "r") as file:
     reading_conditions = True
     for line in file:
         line = line.strip()
@@ -29,8 +29,10 @@ def correct_operations(operations, dependencies):
     total = 0
     for operation in operations:
         if not is_valid_operation(operation, dependencies):
-            # Elements that have less elements depending on them will come first, as keys with lesser value will come first
-            corrected_operation = sorted(operation, key=lambda x: sum(1 for dep in dependencies[x] if dep in operation))
+            # Elements that have more elements depending on them will come first.
+            # Before, I was actually sorted in the inverse order, but as I only used the middle element to add it to the total, I still got the right answer.
+            corrected_operation = sorted(operation, reverse=True, key=lambda x: sum(1 for dep in dependencies[x] if dep in operation))
+            print(corrected_operation)
             total += corrected_operation[len(corrected_operation) // 2]
     return total
 
